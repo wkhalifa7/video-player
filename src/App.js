a41611a13ed62a77b2ef2a679e4e390389308798
Waleed Khalifa <wkhalifa@synapse-analytics.io>
2022-11-12 22:16:35 +0200

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import "./App.css";
-import { useRef, useState } from "react";
-
-import IconButton from "@mui/material/IconButton";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import PauseIcon from "@mui/icons-material/Pause";
-import CircularProgress from "@mui/material/CircularProgress";
-import ReactPlayer from "react-player/file";
-import moment from "moment";
 import VideoPlayer from "./components/VideoPlayer";
 
-// https://tractive.com/assets/static/videos/ActivityMonitoring_15s_EN.mp4
-
 function App() {
-  return (
-    // <div className="App">
-    <VideoPlayer />
-    // </div>
-  );
+  return <VideoPlayer />;
 }
 
 export default App;
